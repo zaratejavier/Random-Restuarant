f712fb35ecf97cabc40a2fcdc97284e07a39c371
Javier Zarate <zarate.javier.jz@gmail.com>
2020-04-23 19:38:59 -0600

    Changed the color of the button

--- main.js
+++ main.js
@@ -5,24 +5,29 @@ var images = new Array(
   'images/burgerKing.png',
   'images/betos.png',
   'images/buffalo.png',
-  'images/cafeRio.jpg',  
-  'images/chickFila.jpg', 
-  'images/costaVida.png',  
-  'images/littleCaesars.png',  
-  'images/panda.png',  
-  'images/pizzahut.png',  
-  'images/zupas.jpg'  
+  'images/cafeRio.jpg',
+  'images/chickFila.jpg',
+  'images/costaVida.png',
+  'images/littleCaesars.png',
+  'images/panda.png',
+  'images/pizzahut.png',
+  'images/zupas.jpg'
 )
 
-function listen(){
-  document.getElementById("random").addEventListener("click",randomPicture)
+
+function listen() {
+  document.getElementById("random").addEventListener("click", randomPicture)
 }
 
-function randomPicture(){
+function randomPicture() {
   for (let i = 0; i < 10; i++) [
     setTimeout(() => {
       const randomPic = images[Math.floor(Math.random() * images.length)];
       document.getElementById("photo").src = randomPic
-    }, 50 * i )
+      
+    }, 50 * i)
   ]
 }
+
+
+
