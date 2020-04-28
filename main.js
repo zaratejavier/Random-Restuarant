7f10c1a14ab3a6c2b3b1046befcae0682229fc1a
Javier Zarate <zarate.javier.jz@gmail.com>
2020-04-27 18:51:24 -0600

    added another picture that displays random restaurants

--- main.js
+++ main.js
@@ -14,9 +14,12 @@ var images = new Array(
   'images/zupas.jpg'
 )
 
+var theResult = document.getElementById('results');
+
 
 function listen() {
   document.getElementById("random").addEventListener("click", randomPicture)
+  document.getElementById("random").addEventListener("click",randomPicture2)
 }
 
 function randomPicture() {
@@ -24,7 +27,15 @@ function randomPicture() {
     setTimeout(() => {
       const randomPic = images[Math.floor(Math.random() * images.length)];
       document.getElementById("photo").src = randomPic
+    }, 50 * i)
+  ]
+}
 
+function randomPicture2() {
+  for (let i = 0; i < 10; i++) [
+    setTimeout(() => {
+      const randomPic = images[Math.floor(Math.random() * images.length)];
+      document.getElementById("photo2").src = randomPic
     }, 50 * i)
   ]
 }
@@ -33,10 +44,21 @@ document.onkeydown = function (event) {
   switch (event.keyCode) {
     case 32:
       randomPicture() // space button
+      // randomPicture2()
       break;
   }
 }
 
+// function displayCounts() {
+//  if(randomPicture() === randomPicture2()){
+//     console.log(
+//       "Thats where youre going"
+//     )
+//   }
+//     else{
+//       console.log("wrong")
+//     }
+//   }
 
-
+//   displayCounts()
 
