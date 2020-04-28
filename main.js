8f5b0a981f13dedfd8aab49f3df27b467689573e
Javier Zarate <zarate.javier.jz@gmail.com>
2020-04-28 16:11:06 -0600

    Diplays the rules

--- main.js
+++ main.js
@@ -1,64 +1,54 @@
-window.addEventListener("load", listen);
+document.addEventListener("DOMContentLoaded", function () {
 
-var images = new Array(
-  'images/mcdonalds.jpg',
-  'images/burgerKing.png',
-  'images/betos.png',
-  'images/buffalo.png',
-  'images/cafeRio.jpg',
-  'images/chickFila.jpg',
-  'images/costaVida.png',
-  'images/littleCaesars.png',
-  'images/panda.png',
-  'images/pizzahut.png',
-  'images/zupas.jpg'
-)
+  var images = new Array(
+    'images/mcdonalds.jpg',
+    'images/burgerKing.png',
+    'images/betos.png',
+    'images/buffalo.png',
+    'images/cafeRio.jpg',
+    'images/chickFila.jpg',
+    'images/costaVida.png',
+    'images/littleCaesars.png',
+    'images/panda.png',
+    'images/pizzahut.png',
+    'images/zupas.jpg'
+  )
 
-var theResult = document.getElementById('results');
+  var theResult = document.getElementById("results");
 
 
-function listen() {
   document.getElementById("random").addEventListener("click", randomPicture)
-  document.getElementById("random").addEventListener("click",randomPicture2)
-}
+  document.getElementById("random").addEventListener("click", randomPicture2)
+  document.getElementById("random").addEventListener("click", myfunction)
 
-function randomPicture() {
-  for (let i = 0; i < 10; i++) [
-    setTimeout(() => {
-      const randomPic = images[Math.floor(Math.random() * images.length)];
-      document.getElementById("photo").src = randomPic
-    }, 50 * i)
-  ]
-}
-
-function randomPicture2() {
-  for (let i = 0; i < 10; i++) [
-    setTimeout(() => {
-      const randomPic = images[Math.floor(Math.random() * images.length)];
-      document.getElementById("photo2").src = randomPic
-    }, 50 * i)
-  ]
-}
-
-document.onkeydown = function (event) {
-  switch (event.keyCode) {
-    case 32:
-      randomPicture() // space button
-      // randomPicture2()
-      break;
+  function randomPicture() {
+    for (let i = 0; i < 10; i++) [
+      setTimeout(() => {
+        const randomPic = images[Math.floor(Math.random() * images.length)];
+        document.getElementById("photo").src = randomPic
+      }, 50 * i)
+    ]
   }
-}
 
-// function displayCounts() {
-//  if(randomPicture() === randomPicture2()){
-//     console.log(
-//       "Thats where youre going"
-//     )
-//   }
-//     else{
-//       console.log("wrong")
-//     }
-//   }
+  function randomPicture2() {
+    for (let i = 0; i < 10; i++) [
+      setTimeout(() => {
+        const randomPic = images[Math.floor(Math.random() * images.length)];
+        document.getElementById("photo2").src = randomPic
+      }, 50 * i)
+    ]
+  }
 
-//   displayCounts()
+  function myfunction(){
+    document.getElementById("results").innerHTML = "If two match, that's where you are going. NO CHEATING!!"
+  }
 
+  document.onkeydown = function (event) {
+    switch (event.keyCode) {
+      case 32:
+        randomPicture() // space button
+        randomPicture2()
+        break;
+    }
+  }
+})
